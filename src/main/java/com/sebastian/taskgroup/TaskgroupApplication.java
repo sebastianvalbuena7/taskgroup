d17b51004e057d13412138ccfc2f1229e80d67b2
Sebastian Valbuena <sebastianvalbuena25@gmail.com>
2023-05-17 17:13:02 -0500

    Dependencias agregadas al proyecto

---
+++ src/main/java/com/sebastian/taskgroup/TaskgroupApplication.java
@@ -0,0 +1,13 @@
+package com.sebastian.taskgroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskgroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskgroupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sebastian/taskgroup/TaskgroupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebastian.taskgroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskgroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
